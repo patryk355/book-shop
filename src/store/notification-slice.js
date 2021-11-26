adf45f71ceeee66836dd4b352e9caf8d25e556e6
Patryk Karasiński <patrykkarasinski11@gmail.com>
2021-11-26 20:52:30 +0100

    add new reducer

--- src/store/notification-slice.js
+++ src/store/notification-slice.js
@@ -5,11 +5,19 @@ const notificationSlice = createSlice({
     initialState: {
         type: '',
         content: '',
+        showNotification: false,
     },
     reducers: {
         showNotification(state, action) {
+            state.showNotification = true;
+
             state.type = action.payload.type;
             state.content = action.payload.content;
+        },
+        hideNotification(state) {
+            state.showNotification = false;
+            state.type = '';
+            state.content = '';
         },
     }
 });
