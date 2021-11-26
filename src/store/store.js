5c286aac3ac0e7912fb87f4a8a9bbe3bbc2fe4af
Patryk Karasiński <patrykkarasinski11@gmail.com>
2021-11-26 19:55:21 +0100

    add notificationSlice to store

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import booksSlice from './books-slice';
 import cartSlice from './cart-slice';
 import loginSlice from './login-slice';
+import notificationSlice from './notification-slice';
 import uiSlice from './ui-slice';
 import validationSlice from './validation-slice';
 
@@ -12,6 +13,7 @@ const store = configureStore({
         validation: validationSlice.reducer,
         books: booksSlice.reducer,
         cart: cartSlice.reducer,
+        notification: notificationSlice.reducer,
     },
 });
 
