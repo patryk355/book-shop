c701ef1758717aa6d018c9c009ad1c735b97a28d
Patryk Karasiński <patrykkarasinski11@gmail.com>
2021-11-26 19:11:30 +0100

    change className logic

--- src/components/UI/Notification/Notification.js
+++ src/components/UI/Notification/Notification.js
@@ -1,19 +1,29 @@
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import styles from './Notification.module.css';
 
-const Notification = ({ children }) => {
+const Notification = ({ children, type }) => {
     const [showNotification, setShowNotification] = useState(true);
 
     const hideNotificationHandler = () => {
         setShowNotification(false);
     }
 
+    let classes;
+
+    if (type === 'error') {
+        classes = styles.error;
+    } else if (type === 'success') {
+        classes = styles.success;
+    } else {
+        classes = styles.info;
+    }
+
     return (
-        <>
-            {showNotification && <div className={styles.notification} onClick={hideNotificationHandler}>
+        <Fragment>
+            {showNotification && <div className={classes} onClick={hideNotificationHandler} type={type}>
                 {children}
             </div>}
-        </>
+        </Fragment >
     )
 }
 
